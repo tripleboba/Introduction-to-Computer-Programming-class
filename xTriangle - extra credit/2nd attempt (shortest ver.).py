e820239828f68b6f58e24df671d23060e82e4efb
Phuong Anh <phganh@users.noreply.github.com>
2016-12-18 03:23:41 -0800

    Create 2nd attempt (shortest ver.).py

---
+++ xTriangle - extra credit/2nd attempt (shortest ver.).py
@@ -0,0 +1,12 @@
+def xTriangle():
+
+    intHeight = int(input("Enter triangle's height: "))
+    
+    for i in range(intHeight):
+        print( "X" * (i+1) )    # start @ 0 -> +1 to start printing 1st X
+
+xTriangle()
+
+
+
+    
